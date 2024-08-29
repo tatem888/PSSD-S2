225b2fd7cf5aa1c7aa5cafb1f738d4a8c9e45788
Tate Manning <tatemanning@icloud.com>
2024-08-29 18:21:30 +0930

    check

--- test.cpp
+++ test.cpp
@@ -1,10 +1,10 @@
-#include "Multiples.hpp"
+#include "Multiples/Multiples.hpp"
 #include <iostream>
 
 int main(){
 
     Multiples a;
-    std::cout << a.number(100,5812,8);
+    std::cout << a.number(-75312,407891,14);
 
     
 }
