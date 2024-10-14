30839b1d206870f94bec8785d71aa474f423b18a
Tate Manning <tatemanning@icloud.com>
2024-10-14 20:53:32 +1030

    thimbles done

---
+++ week9/Thimbles/Thimbles.hpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <string>
+
+class Thimbles
+{
+    
+public:
+
+    int thimbleWithBall(std::vector<std::string> swaps){
+
+        int currentPosition = 1;
+
+        for(int i = 0; i < swaps.size(); i++){
+            
+            std::string currentSwapString = swaps[i];
+
+            int num1 = currentSwapString[0];
+            int num2 = currentSwapString[2];
+
+            if (currentPosition == num1){
+                currentPosition = num2;
+            }
+            else if (currentPosition == num2){
+                currentPosition = num1;
+            }
+ 
+        }
+
+        return currentPosition;
+       
+    }
+    
+};
+
