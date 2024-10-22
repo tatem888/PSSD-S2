11a7c17a42f45809b5727a29a811f7164411337e
Tate Manning <tatemanning@icloud.com>
2024-10-22 11:44:35 +1030

    string diff doine

---
+++ week10/DifferentStrings/DifferentStrings.hpp
@@ -0,0 +1,42 @@
+#include <string>
+
+class DifferentStrings
+{
+private:
+    
+public:
+
+    int minimize(std::string A, std::string B){
+
+        //compare a to substrings of b
+
+        int ALength = A.length();
+        int BLength = B.length();
+
+        int numberIter = BLength - ALength + 1;
+        int minDifference = ALength;
+
+        for (int  i = 0; i < numberIter; i++)
+        {
+            int substringDifference = 0;
+
+            for (int j = 0; i < ALength; i++)
+            {
+                if (A[j] != B[i+j])
+                {
+                    substringDifference++;
+                }
+                
+            }
+
+            if (substringDifference < minDifference){
+                minDifference = substringDifference;
+            }
+        }
+
+        return minDifference;
+        
+        
+    }
+    
+};
